b746001ca07290a1f62a028df85da1eb84a1b726
Paulina Gruzewska <paula@wildland.io>
2023-08-06 17:14:09 +0200

    clean up main wien

--- meadow.uitesting.mockup/ContentView.swift
+++ meadow.uitesting.mockup/ContentView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-        }
-        .padding()
-    }
+        Text("")
 }
 
 struct ContentView_Previews: PreviewProvider {
