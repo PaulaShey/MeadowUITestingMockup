f3055ebad102313dc37d6fa8f8b8fa7cfecf33b3
Paulina Gruzewska <paula@wildland.io>
2023-09-01 16:48:11 +0200

    Update post build shell scripts contents

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,4 +4,10 @@
 #  Meadow UI Testing Mockup
 #
 #  Created by Paulina Grużewska on 01/09/2023.
-#  
+#  # Define paths
+# Define paths
+DOCC_ARCHIVE_PATH="${BUILD_DIR}/Meadow UI Testing Mockup.doccarchive"
+OUTPUT_PATH="${SOURCE_ROOT}/docs"
+
+# Run docc to process and transform the documentation archive
+$(xcrun --find docc) process-archive transform-for-static-hosting $DOCC_ARCHIVE_PATH --output-path $OUTPUT_PATH
