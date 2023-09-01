2c62812d305231c7188bed3e7f17afca5f321352
Paulina Gruzewska <paula@wildland.io>
2023-09-01 17:14:12 +0200

    Update post build shell script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -6,10 +6,15 @@
 #  Created by Paulina Grużewska on 01/09/2023.
 #  # Define paths
 # Define paths
-DOCC_ARCHIVE_PATH="${BUILD_DIR}/Meadow UI Testing Mockup.doccarchive"
-OUTPUT_PATH="${SOURCE_ROOT}/docs"
+#!/bin/sh
+
+# Define paths
+DOCC_ARCHIVE_PATH="${BUILD_DIR}/MyApp.doccarchive"
+TEMP_OUTPUT_PATH="${TMPDIR}/docs"
+DESTINATION_PATH="${SOURCE_ROOT}/docs"
 
 # Run docc to process and transform the documentation archive
-$(xcrun --find docc) process-archive transform-for-static-hosting $DOCC_ARCHIVE_PATH --output-path $OUTPUT_PATH
+$(xcrun --find docc) process-archive transform-for-static-hosting $DOCC_ARCHIVE_PATH --output-path $TEMP_OUTPUT_PATH
 
-cat "${TEMP_OUTPUT_PATH}/index.html" > "${SOURCE_ROOT}/index.html"
+# Copy the entire transformed documentation to the destination directory
+rsync -av --delete "${TEMP_OUTPUT_PATH}/" "${DESTINATION_PATH}/"
