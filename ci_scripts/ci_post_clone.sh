353b7af157b985b5019c5a00b91e9c3005bd778e
Paulina Gruzewska <paula@wildland.io>
2023-09-04 09:34:23 +0200

    s

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,22 +5,25 @@
 #
 #  Created by Paulina Grużewska on 01/09/2023.
 # # Define paths
-DOCC_ARCHIVE_PATH="${BUILD_DIR}/Meadow UI Testing Mockup.doccarchive"
-TEMP_OUTPUT_PATH="${TMPDIR}/docs"
-DESTINATION_PATH="${SOURCE_ROOT}/docs"
+DOCC_ARCHIVE_PATH="${BUILD_DIR}../Meadow UI Testing Mockup.doccarchive"
+DESTINATION_PATH="./docs"
+DESTINATION_PATH="./docs"
+
+
+xcodebuild doccbuild -scheme meadow.uitesting.mockup -destination 'platform=iOS Simulator,name=iPhone 14'
+
 
 # Run docc to process and transform the documentation archive
-#$(xcrun --find docc) process-archive transform-for-static-hosting $DOCC_ARCHIVE_PATH --output-path $TEMP_OUTPUT_PATH
+$(xcrun --find docc) process-archive transform-for-static-hosting $DOCC_ARCHIVE_PATH --output-path $TEMP_OUTPUT_PATH
 
 # Copy the entire transformed documentation to the destination directory
-#rsync -av --delete "${TEMP_OUTPUT_PATH}/" "${DESTINATION_PATH}/"
+rsync -av --delete "${TEMP_OUTPUT_PATH}/" "${DESTINATION_PATH}/"
 
 # Change to the directory where the Git repository is
-echo "Current Directory: $(pwd)"
-echo "source root = ${SOURCE_ROOT}"
-echo "destination path = ${DESTINATION_PATH}"
-echo  "docc archive path = ${DOCC_ARCHIVE_PATH}"
-echo "BUILD_DIR: $BUILD_DIR"
-echo "SRCROOT: $SRCROOT"
-
-echo "repos = $(git remote -v)"
+ cd ..
+ 
+ git add .
+ 
+ git commit -m "message"
+ 
+ git push origin main
